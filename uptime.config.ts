57a57129539088400d561bc6571b1a85793a94f7
Aocro <149652962+Aocro@users.noreply.github.com>
2024-07-27 15:46:38 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,11 +1,9 @@
 const pageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Aocro Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
   ],
 }
 
@@ -14,57 +12,77 @@ const workerConfig = {
   kvWriteCooldownMinutes: 3,
   // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'foo_monitor',
-      // `name` is used at status page and callback message
-      name: 'My API Monitor',
-      // `method` should be a valid HTTP Method
-      method: 'POST',
-      // `target` is a valid URL
-      target: 'https://example.com',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://example.com',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      headers: {
-        'User-Agent': 'Uptimeflare',
-        Authorization: 'Bearer YOUR_TOKEN_HERE',
-      },
-      // [OPTIONAL] body to be sent
-      body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      checkLocationWorkerRoute: 'https://xxx.example.com',
+      id: 'whoogle',
+      name: 'Search',
+      method: 'GET',
+      target: 'https://s.aocro.com',
+      tooltip: 'Aocro Search',
     },
-    // Example TCP Monitor
     {
-      id: 'test_tcp_monitor',
-      name: 'Example TCP Monitor',
-      // `method` should be `TCP_PING` for tcp monitors
-      method: 'TCP_PING',
-      // `target` should be `host:port` for tcp monitors
-      target: '1.2.3.4:22',
-      tooltip: 'My production server SSH',
-      statusPageLink: 'https://example.com',
-      timeout: 5000,
+      id: 'gitea',
+      name: 'Git',
+      method: 'GET',
+      target: 'https://git.aocro.com',
+      tooltip: 'Aocro Git',
+    },
+    {
+      id: 'ittools',
+      name: 'Tools',
+      method: 'GET',
+      target: 'https://tools.aocro.com',
+      tooltip: 'Aocro Tools',
+    },
+    {
+      id: 'blog',
+      name: 'Blog',
+      method: 'GET',
+      target: 'https://blog.aocro.com',
+      tooltip: 'Aocro Blog',
+    },
+    {
+      id: 'excalidraw',
+      name: 'Draw',
+      method: 'GET',
+      target: 'https://draw.aocro.com',
+      tooltip: 'Aocro Draw',
+    },
+    {
+      id: 'drawio',
+      name: 'Drawio',
+      method: 'GET',
+      target: 'https://drawio.aocro.com',
+      tooltip: 'Aocro Drawio',
+    },
+    {
+      id: 'rsshub',
+      name: 'Rss',
+      method: 'GET',
+      target: 'https://rss.aocro.com',
+      tooltip: 'Aocro Rss',
+    },
+    {
+      id: 'authelia',
+      name: 'Auth',
+      method: 'GET',
+      target: 'https://auth.aocro.com',
+      tooltip: 'Aocro Auth',
+    },
+    {
+      id: 'home',
+      name: 'Home',
+      method: 'GET',
+      target: 'https://www.aocro.com',
+      tooltip: 'Aocro Home',
     },
   ],
   notification: {
     // [Optional] apprise API server URL
     // if not specified, no notification will be sent
-    appriseApiServer: "https://apprise.example.com/notify",
+    appriseApiServer: "",
     // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
     // if not specified, no notification will be sent
-    recipientUrl: "tgram://bottoken/ChatID",
+    recipientUrl: "",
     // [Optional] timezone used in notification messages, default to "Etc/GMT"
     timeZone: "Asia/Shanghai",
     // [Optional] grace period in minutes before sending a notification
@@ -86,6 +104,26 @@ const workerConfig = {
 
       // This will not follow the grace period settings and will be called immediately when the status changes
       // You need to handle the grace period manually if you want to implement it
+      const timeString = new Date(timeNow * 1000).toLocaleString('en-US', {
+        timeZone: 'Asia/Taipei',
+      })
+      let statusChangeMsg
+      if (isUp) {
+        statusChangeMsg = `✔️${name} came back up at ${timeString} after ${Math.round(
+          (timeNow - timeIncidentStart) / 60
+        )} minutes of downtime`
+      } else {
+        statusChangeMsg = `❌${name} was down at ${timeString} with error ${reason}`
+      }
+      await fetch('https://ntfy.sh/status-agreement6969', {
+          method: 'POST',
+          body: statusChangeMsg,
+          headers: {
+              'Title': 'Aocro Status Changed',
+              'Priority': 'urgent',
+              'Tags': 'warning'
+          }
+      })
     },
     onIncident: async (
       env: any,
